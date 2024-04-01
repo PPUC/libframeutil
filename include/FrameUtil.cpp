f187091a1dfeea0c51697e55836c27e87fee9921
Markus Kalkbrenner <git@kalki.de>
2024-04-01 16:37:55 +0200

    initial commit

---
+++ include/FrameUtil.cpp
@@ -0,0 +1,18 @@
+/*
+ * Portions of this code was derived from DMDExt
+ *
+ * https://github.com/freezy/dmd-extensions/blob/master/LibDmd/Common/FrameUtil.cs
+ */
+
+#include "FrameUtil.h"
+
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+namespace DMDUtil
+{
+
+
+}  // namespace DMDUtil
